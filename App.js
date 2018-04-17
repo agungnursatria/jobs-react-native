162acbe2ae16bbfcde4b1d4eeb15ec63cfbf6e0e
Agung Nursatria <agungnursatria@gmail.com>
2018-04-18 01:02:54 +0700

    finish video 150 & done all

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import { Notifications } from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, Alert } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 
+import registerForNotifications from './services/push_notifications';
 import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -12,6 +14,21 @@ import SettingsScreen from './screens/SettingsScreen';
 import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
+  componentDidMount(){
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+
+      if(origin === 'received' && text){
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok.' }]
+        );
+      }      
+    });
+  }
+  
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
@@ -30,7 +47,8 @@ export default class App extends React.Component {
           tabBarPosition: 'bottom',
           swipeEnabled: false,
           tabBarOptions: {
-            labelStyle: { fontSize: 12 }
+            labelStyle: { fontSize: 12 },
+            showIcon: true
           }
         })
       }
